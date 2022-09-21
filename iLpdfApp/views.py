58ae59fc140a4dd6bcf16526707e84d96ab796b4
Sayem475 <sayemuddin471@gmail.com>
2022-09-21 15:53:55 +0600

    added some page

--- iLpdfApp/views.py
+++ iLpdfApp/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'Home/home.html')
+
+def mergepdf(request):
+    return render(request,'PdfConvert/merge_pdf.html')
